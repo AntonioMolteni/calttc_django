a2a74ca8808652e3924a8ff15f482f3d6dad9521
AntonioMolteni <AntonioMolteni@berkeley.edu>
2025-02-04 15:41:07 -0800

    fixed sessions model causing glitch

--- session/models.py
+++ session/models.py
@@ -4,93 +4,118 @@ from django.utils import timezone
 from accounts.models import User
 
 SessionTypeChoices = (
-  ('NT','Novice Training'),
-  ('IT','Intermediate Training'),
-  ('AT','Advanced Training'),
-  ('RR','Round-Robin Tournament'),
-  ('TO','Tournament'),
-  ('CT','Competitive Team Tryouts'),
+    ("NT", "Novice Training"),
+    ("IT", "Intermediate Training"),
+    ("AT", "Advanced Training"),
+    ("RR", "Round-Robin Tournament"),
+    ("TO", "Tournament"),
+    ("CT", "Competitive Team Tryouts"),
 )
 
 LocationChoices = (
-	('RSFField','RSF Field House'),
-  ('RSFCombat','RSF Combatives Room'),
+    ("RSFField", "RSF Field House"),
+    ("RSFCombat", "RSF Combatives Room"),
 )
 
 # cutoff for session sign_ups in minutes
 cutoff = 5
 
 today = timezone.now().date()
-eight_months_ago = today - timedelta(days=8*30)  # Assuming 30 days per month
+eight_months_ago = today - timedelta(days=8 * 30)  # Assuming 30 days per month
 
 # Filter the members queryset based on the last login date range
 users = User.objects.filter(last_login__range=[eight_months_ago, today])
-    
+
 
 class Session(models.Model):
-  created_at = models.DateTimeField(auto_now_add=True)
-  session_type = models.CharField(max_length=30, choices=SessionTypeChoices, default='')
-  location = models.CharField(max_length=30, choices=LocationChoices, default='RSF Field House')
-  time = models.DateTimeField()
-  duration = models.PositiveIntegerField()
-  capacity = models.IntegerField()
-  note = models.CharField(max_length=300, default=None, blank=True, null=True)
-  is_closed = models.BooleanField(default = False)
-  is_cancelled = models.BooleanField(default=False)
-  coaches = models.ManyToManyField(User, related_name='coaching', limit_choices_to= {'is_staff': True})
-  players = models.ManyToManyField(User, related_name='playing', blank=True)
-  queue = models.ManyToManyField(User, related_name='queuing', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    session_type = models.CharField(
+        max_length=30, choices=SessionTypeChoices, default=""
+    )
+    location = models.CharField(
+        max_length=30, choices=LocationChoices, default="RSF Field House"
+    )
+    time = models.DateTimeField()
+    duration = models.PositiveIntegerField()
+    capacity = models.IntegerField()
+    note = models.CharField(max_length=300, default=None, blank=True, null=True)
+    is_closed = models.BooleanField(default=False)
+    is_cancelled = models.BooleanField(default=False)
+    coaches = models.ManyToManyField(
+        User, related_name="coaching", limit_choices_to={"is_staff": True}
+    )
+    players = models.ManyToManyField(User, related_name="playing", blank=True)
+    queue = models.ManyToManyField(User, related_name="queuing", blank=True)
+    requires_membership = models.BooleanField(default=False)
 
-  def __str__(self):
-    end_time = self.time + timedelta(minutes=int(self.duration))
-    return self.time.strftime("%-I:%M") + "-" + end_time.strftime("%-I:%M %p") + self.time.strftime(" %a %b %-d")
-  
-  def get_players(self):
-    return self.players.filter(is_active=True).order_by('-is_member', '-has_berkeley_email', 'sign_up_date',)[:self.capacity] 
-  
-  def get_wait_list(self):
-    return self.players.filter(is_active=True).order_by('-is_member', '-has_berkeley_email', 'sign_up_date')[self.capacity:]
-  
-  def get_queue(self):
-    return self.queue.filter(is_active=True).order_by('-is_member', '-has_berkeley_email', 'date_joined')[:self.capacity]
-  
-  def get_session_type(self):
-    return "Session Type: " + self.get_session_type_display()
-    
-  def get_coaches(self):
-    if self.coaches.count() > 1:
-      output_string = "Coaches: "
-    else:
-      output_string = "Coach: "
-    comma_string = ", ".join([coach.display_name() for coach in self.coaches.all()])
-    return output_string + comma_string
-  
-  def get_location(self):
-    return "Location: " + self.get_location_display()
-  
-  def get_capacity(self):
-    return "Capacity: " + str(self.capacity)
-  
-  def get_remaining_slots(self):
-    return "Remaining Slots: " + str(max(self.capacity-self.players.count(), 0))
-  
-  def get_id(self):
-    return "session_" + str(self.id)
-  
-  def is_full(self):
-    return self.capacity-self.players.count() <= 0
-  
-  def is_available(self):
-    return datetime.now() <= self.time - timedelta(minutes=cutoff) and not self.is_closed and not self.is_cancelled
+    def __str__(self):
+        end_time = self.time + timedelta(minutes=int(self.duration))
+        return (
+            self.time.strftime("%-I:%M")
+            + "-"
+            + end_time.strftime("%-I:%M %p")
+            + self.time.strftime(" %a %b %-d")
+        )
 
-  def is_training(self):
-    return self.session_type == "NT" or self.session_type == "IT" or self.session_type == "AT"
-  
-  def is_competitive_team_tryouts(self):
-    return self.session_type == "CT"
-    
-  def is_tournament(self):
-    return self.session_type == "RR" or self.session_type == "TO"
-  
+    def get_players(self):
+        return self.players.filter(is_active=True).order_by(
+            "-is_member",
+            "-has_berkeley_email",
+            "sign_up_date",
+        )[: self.capacity]
 
-  
+    def get_wait_list(self):
+        return self.players.filter(is_active=True).order_by(
+            "-is_member", "-has_berkeley_email", "sign_up_date"
+        )[self.capacity :]
+
+    def get_queue(self):
+        return self.queue.filter(is_active=True).order_by(
+            "-is_member", "-has_berkeley_email", "date_joined"
+        )[: self.capacity]
+
+    def get_session_type(self):
+        return "Session Type: " + self.get_session_type_display()
+
+    def get_coaches(self):
+        if self.coaches.count() > 1:
+            output_string = "Coaches: "
+        else:
+            output_string = "Coach: "
+        comma_string = ", ".join([coach.display_name() for coach in self.coaches.all()])
+        return output_string + comma_string
+
+    def get_location(self):
+        return "Location: " + self.get_location_display()
+
+    def get_capacity(self):
+        return "Capacity: " + str(self.capacity)
+
+    def get_remaining_slots(self):
+        return "Remaining Slots: " + str(max(self.capacity - self.players.count(), 0))
+
+    def get_id(self):
+        return "session_" + str(self.id)
+
+    def is_full(self):
+        return self.capacity - self.players.count() <= 0
+
+    def is_available(self):
+        return (
+            datetime.now() <= self.time - timedelta(minutes=cutoff)
+            and not self.is_closed
+            and not self.is_cancelled
+        )
+
+    def is_training(self):
+        return (
+            self.session_type == "NT"
+            or self.session_type == "IT"
+            or self.session_type == "AT"
+        )
+
+    def is_competitive_team_tryouts(self):
+        return self.session_type == "CT"
+
+    def is_tournament(self):
+        return self.session_type == "RR" or self.session_type == "TO"
--- session/views.py
+++ session/views.py
@@ -9,11 +9,19 @@ from datetime import datetime, timedelta
 from .forms import AddSessionsForm, EditSessionForm
 from django.forms import modelformset_factory, TextInput
 
+
 def refresh_queries():
     global all_available_sessions
     global all_sessions
-    all_available_sessions = Session.objects.filter(time__gte=datetime.now() + timedelta(minutes=cutoff)).exclude(is_closed=True).exclude(is_cancelled=True)
-    all_sessions = Session.objects.filter(time__gte=datetime.now()-timedelta(hours=3)).order_by("time")
+    all_available_sessions = (
+        Session.objects.filter(time__gte=datetime.now() + timedelta(minutes=cutoff))
+        .exclude(is_closed=True)
+        .exclude(is_cancelled=True)
+    )
+    all_sessions = Session.objects.filter(
+        time__gte=datetime.now() - timedelta(hours=3)
+    ).order_by("time")
+
 
 # helper function to determine if user has already signed up or has been queued and returns a tuple of boolean values
 def has_signed_up(request):
@@ -21,54 +29,69 @@ def has_signed_up(request):
     already_queued = False
     if request.user.is_authenticated:
         # exclude tournaments so that you can still sign up for multiple sessions
-        for session in all_available_sessions.exclude(session_type="RR").exclude(session_type="TO"):
+        for session in all_available_sessions.exclude(session_type="RR").exclude(
+            session_type="TO"
+        ):
             if session.players.contains(request.user):
                 already_signed_up = True
             if session.queue.contains(request.user):
                 already_queued = True
     return (already_signed_up, already_queued)
 
+
 def sessions(request):
-    page_title = 'Sessions'
+    page_title = "Sessions"
     refresh_queries()
-    already_signed_up, already_queued = has_signed_up(request)[0], has_signed_up(request)[1]
+    already_signed_up, already_queued = (
+        has_signed_up(request)[0],
+        has_signed_up(request)[1],
+    )
     # Update roster
     for session in all_sessions:
-        if session.get_queue() and datetime.now() >= session.time - timedelta(minutes=cutoff):
-            extra_space = max(0,session.capacity - session.players.count())
-            for user in session.queue.order_by('-is_member', '-has_berkeley_email', 'sign_up_date',)[:extra_space].all():
+        if session.get_queue() and datetime.now() >= session.time - timedelta(
+            minutes=cutoff
+        ):
+            extra_space = max(0, session.capacity - session.players.count())
+            for user in session.queue.order_by(
+                "-is_member",
+                "-has_berkeley_email",
+                "sign_up_date",
+            )[:extra_space].all():
                 user_signed_up = False
                 for s in all_available_sessions:
                     if s.players.contains(user):
-                        user_signed_up = True 
+                        user_signed_up = True
                 if not user_signed_up:
                     session.players.add(user)
                     session.queue.remove(user)
                     already_queued = False
-    
-    announcements = Announcement.objects.filter(on_sessions_page=True)       
-    
-    return render(request, 'sessions/sessions.html',
+
+    announcements = Announcement.objects.filter(on_sessions_page=True)
+
+    return render(
+        request,
+        "sessions/sessions.html",
         {
-        'page_title': page_title,
-        'cutoff': cutoff,
-        'all_sessions': all_sessions,
-        'already_signed_up': already_signed_up,
-        'already_queued': already_queued,
-        'announcements': announcements,
-        }
+            "page_title": page_title,
+            "cutoff": cutoff,
+            "all_sessions": all_sessions,
+            "already_signed_up": already_signed_up,
+            "already_queued": already_queued,
+            "announcements": announcements,
+        },
     )
+
 
 @login_required
 @staff_member_required
 def add_sessions(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = AddSessionsForm(request.POST)
 
         if form.is_valid():
-            total_duration = form.cleaned_data['total_duration']
-            num_sessions = form.cleaned_data['num_sessions']
-            start_time = form.cleaned_data['start_time']
+            total_duration = form.cleaned_data["total_duration"]
+            num_sessions = form.cleaned_data["num_sessions"]
+            start_time = form.cleaned_data["start_time"]
 
             duration_per_session = total_duration // num_sessions
             session_time = start_time
@@ -77,84 +100,105 @@ def add_sessions(request):
 
             for _ in range(num_sessions):
                 session = Session(
-                    session_type=form.cleaned_data['session_type'],
-                    location=form.cleaned_data['location'],
+                    session_type=form.cleaned_data["session_type"],
+                    location=form.cleaned_data["location"],
                     time=session_time,
                     duration=duration_per_session,
-                    capacity=form.cleaned_data['capacity'],
-                    note=form.cleaned_data['note'],
+                    capacity=form.cleaned_data["capacity"],
+                    note=form.cleaned_data["note"],
                 )
                 session.save()
-                session.coaches.set(form.cleaned_data['coaches'])
+                session.coaches.set(form.cleaned_data["coaches"])
                 sessions.append(session)
 
                 session_time += timedelta(minutes=duration_per_session)
 
-            return redirect(reverse('sessions')+'#'+sessions[0].get_id())
+            return redirect(reverse("sessions") + "#" + sessions[0].get_id())
     else:
         form = AddSessionsForm()
 
-    return render(request, 'sessions/add_sessions.html', {'form': form})
+    return render(request, "sessions/add_sessions.html", {"form": form})
 
 
 @login_required
 @staff_member_required
 def edit_session(request, session_id):
-    session= Session.objects.get(pk=session_id)
+    session = Session.objects.get(pk=session_id)
     form = EditSessionForm(request.POST or None, instance=session)
     if form.is_valid():
         form.save()
-        return redirect(reverse('sessions')+'#'+session.get_id())
+        return redirect(reverse("sessions") + "#" + session.get_id())
 
-    return render(request, 'sessions/edit_session.html',
-                  {'session': session,
-                   'form': form})
-    
+    return render(
+        request, "sessions/edit_session.html", {"session": session, "form": form}
+    )
+
+
 @login_required
 @staff_member_required
 def delete_session(request, session_id):
     session = Session.objects.get(pk=session_id)
     session.delete()
-    return redirect(reverse('sessions')+'#sign-up')
+    return redirect(reverse("sessions") + "#sign-up")
+
 
 @login_required
 @staff_member_required
 def roster_by_rating(request, session_id):
     session = Session.objects.get(pk=session_id)
-    players = session.players.order_by('-rating')
-    UserFormSet = modelformset_factory(User, fields=('rating',), widgets={"rating": TextInput(attrs={'class':'form-control rating','type':'tel'},)}, extra=0)
+    players = session.players.order_by("-rating")
+    UserFormSet = modelformset_factory(
+        User,
+        fields=("rating",),
+        widgets={
+            "rating": TextInput(
+                attrs={"class": "form-control rating", "type": "tel"},
+            )
+        },
+        extra=0,
+    )
     if request.method == "POST":
         formset = UserFormSet(
-            request.POST, request.FILES,
+            request.POST,
+            request.FILES,
             queryset=players,
         )
         if formset.is_valid():
             formset.save()
-            return redirect(reverse('sessions')+'#'+session.get_id())
+            return redirect(reverse("sessions") + "#" + session.get_id())
     else:
         formset = UserFormSet(queryset=players)
-    return render(request, 'sessions/roster_by_rating.html',
-        {'formset':formset,
-        })
+    return render(
+        request,
+        "sessions/roster_by_rating.html",
+        {
+            "formset": formset,
+        },
+    )
+
 
 @login_required
 def drop(request, session_id):
     session = Session.objects.get(pk=session_id)
     session.players.remove(request.user)
     session.queue.remove(request.user)
-    return redirect(reverse('sessions')+'#'+session.get_id())
+    return redirect(reverse("sessions") + "#" + session.get_id())
+
 
 @login_required
 def sign_up(request, session_id):
     refresh_queries()
-    already_signed_up, already_queued = has_signed_up(request)[0], has_signed_up(request)[1]
-    
+    already_signed_up, already_queued = (
+        has_signed_up(request)[0],
+        has_signed_up(request)[1],
+    )
+
     session = Session.objects.get(pk=session_id)
     if session.is_available():
         if session.is_tournament():
             session.players.add(request.user)
         elif session.is_competitive_team_tryouts() and request.user.rating == 0:
-            return redirect(reverse('sessions') + '?no_rating=True')
+            return redirect(reverse("sessions") + "?no_rating=True")
         elif session.is_competitive_team_tryouts():
             session.players.add(request.user)
         else:
@@ -164,10 +208,9 @@ def sign_up(request, session_id):
                 session.players.add(request.user)
             if already_signed_up and not already_queued:
                 session.queue.add(request.user)
-            if session.queue.contains(request.user) and session.players.contains(request.user):
+            if session.queue.contains(request.user) and session.players.contains(
+                request.user
+            ):
                 session.queue.remove(request.user)
-            
-        
-        
-    return redirect(reverse('sessions') + '#' + session.get_id())
 
+    return redirect(reverse("sessions") + "#" + session.get_id())
